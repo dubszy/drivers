ac4834513c3c9ed675713596f276ce4ecde9d2b5
Martin Waltman <martin@mkwiseman.com>
2018-05-27 17:18:22 -0700

    Make ip_types functions inline

--- include/types/ip_types.h
+++ include/types/ip_types.h
@@ -14,20 +14,20 @@ typedef struct {
     uint8_t d;
 } IPAddress;
 
-in_addr_t ipton(IPAddress ipAddress) {
+inline in_addr_t ipton(IPAddress ipAddress) {
     return (ipAddress.a << 24) +
            (ipAddress.b << 16) +
            (ipAddress.c << 8) +
            (ipAddress.d);
 }
 
-int ip_address_to_cstr(IPAddress ipAddress, char *cstr) {
+inline int ip_address_to_cstr(IPAddress ipAddress, char *cstr) {
     return sprintf(cstr, "%d.%d.%d.%d", ipAddress.a, ipAddress.b, ipAddress.c, ipAddress.d);
 }
 
 #ifdef __cplusplus
 #include <string>
-std::string ip_address_to_string(IPAddress ipAddress) {
+inline std::string ip_address_to_string(IPAddress ipAddress) {
     char cstr[16];
     ip_address_to_cstr(ipAddress, cstr);
     return std::string(cstr);
